91283c40e9768d4db8637bf0fd863f5324a5e86f
Khang <letankhang2@gmail.com>
2017-12-31 16:15:35 +0700

    met moi lan 2

--- WebTTTH/src/DAO/AccountDAO.java
+++ WebTTTH/src/DAO/AccountDAO.java
@@ -33,42 +33,5 @@ public class AccountDAO {
         }
 		return flag;
 	}
-	public static Account LoadAccountbyAccountID(int account_id) throws SQLException{
-		Connection conn = DBConnection.CreateConnection();	
-		Account user= new Account();
-		String str="SELECT * FROM account WHERE account_id=? ";
-		
-		PreparedStatement pstm = conn.prepareStatement(str);
-        pstm.setInt(1, account_id);
-        ResultSet rs = pstm.executeQuery();
-		
-		//Kiểm tra user và pass
-		if (rs.next()) {
-           	
-			Integer account_id1=rs.getInt("account_id");
-			String accountname=rs.getString("accountname");
-			String avatar=rs.getString("avatar");
-			Integer role_id= rs.getInt("role_id");
-			String name= rs.getString("name");
-			String birthday=rs.getString("birthday");
-			String sex= rs.getString("sex");
-			String mail=rs.getString("mail");
-			String phonenumber=rs.getString("phonenumber");
-            	
-			user.setAccount_id(account_id1);
-			user.setAccountname(accountname);
-			user.setAvatar(avatar);
-			user.setRole_id(role_id);
-			user.setName(name);
-			user.setBirthday(birthday);
-			user.setSex(sex);
-			user.setMail(mail);
-			user.setPhonenumber(phonenumber);
-			
-			conn.close();
-			return user;
-        }
-		return null;
-	}
 
 }
